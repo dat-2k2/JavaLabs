2f55b64c0f1ce7b9c22d5a60e640efb10d657702
Nguyen Quoc Dat <forwork.dat2002@gmail.com>
2023-11-23 12:13:07 +0300

    Fix AppTest

--- src/test/java/ru/spbstu/telematics/java/AppTest.java
+++ src/test/java/ru/spbstu/telematics/java/AppTest.java
@@ -2,30 +2,68 @@ package ru.spbstu.telematics.java;
 import org.apache.commons.cli.ParseException;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class AppTest {
+
+    File testFile = new File("~tmp.txt");
     @Test
     public void testLab1Normal() {
         try{
-            App.actionLab1(new String[]{"-ow", "-f", "abc.txt", "-d", "Hello world"});
+            assert(testFile.createNewFile());
+            App.actionLab1(new String[]{"-ow", "-f", testFile.getName(), "-d", "Hello world"});
+            assert(true);
         }
-        catch (Exception e){
-            return;
+        catch (FileNotFoundException e){
+            System.out.println("Error in overwrite function");
         }
-        assert(true);
+        catch (IOException e){
+            System.out.println("Create test file " + testFile.getName() + " failed");
+        }
+        catch (ParseException e){
+            System.out.println("Error while execute CmdLine");
+        }
+        finally {
+            assert(testFile.delete());
+        }
+
     }
 
     @Test(expected = ParseException.class)
-    public void testNoFileArg() throws ParseException, FileNotFoundException{
-        App.actionLab1(new String[]{"-ow", "-d", "Hello world"});
+    public void testNoFileArg() throws ParseException{
+        try {
+            assert(testFile.createNewFile());
+            App.actionLab1(new String[]{"-ow", "-d", "Hello world"});
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Error in overwrite function");
+        }
+        catch (IOException e){
+            System.out.println("Create test file " + testFile.getName() + " failed");
+        }
+        finally {
+            assert(testFile.delete());
+        }
+
     }
 
     @Test(expected = ParseException.class)
     public void testNoDataArg() throws ParseException, FileNotFoundException{
-        App.actionLab1(new String[]{"-ow", "-f", "abc.txt"});
+        try {
+            assert(testFile.createNewFile());
+            App.actionLab1(new String[]{"-ow", "-f", "abc.txt"});
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Error in overwrite function");
+        }
+        catch (IOException e){
+            System.out.println("Create test file " + testFile.getName() + " failed");
+        }
+        finally {
+            assert(testFile.delete());
+        }
     }
-
-
 }
